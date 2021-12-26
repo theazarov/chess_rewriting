c1ef2faf9b53462132f88f49fff1d5d71e2e9c32
theazarov <theazarov@gmail.com>
2021-12-26 21:52:30 +0300

    lesson 2

--- chess_new/main.js
+++ chess_new/main.js
@@ -4,11 +4,26 @@ let board = document.getElementById('board')
 function show_map(board){
     let table = document.createElement('table')
 
-    for(let y = 7; y >= 0; y--){
+    for(let y = 8; y >= 0; y--){
         let tr = document.createElement('tr')
 
-        for(let x = 0; x <= 7; x++){
+        for(let x = 0; x <= 8; x++){
+            
             let td = document.createElement('td')
+            let color = (x + y) % 2 ? td.classList.add('white') : td.classList.add('black')
+
+            if(x == 0){
+                td.removeAttribute("class")
+                td.innerHTML = 'y' + y
+                td.classList.add('coordinate')
+            }
+
+            if(y == 0){
+                td.removeAttribute("class")
+                td.innerHTML = 'x' + x
+                td.classList.add('coordinate')
+            }
+
             tr.appendChild(td)
         }
         table.appendChild(tr)
@@ -16,3 +31,4 @@ function show_map(board){
     board.appendChild(table)
 }
 show_map(board)
+
