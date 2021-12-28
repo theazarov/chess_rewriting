0fdee35cf160ac0ed484803752eba32496502997
theazarov <theazarov@gmail.com>
2021-12-28 19:19:37 +0300

    hi

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let color;
 let theMap = Array();
 let theInf = Array();
 let move_color = "white";
+let move_from_x;
+let move_from_y;
 
 function init_inf() {
   theInf = [
@@ -45,11 +47,31 @@ function mark_moves_from() {
       }
     }
   }
-  console.log(theInf);
+}
+
+function mark_moves_to() {
+  init_inf();
+
+  for (let x = 0; x <= 8; x++) {
+    for (let y = 0; y <= 8; y++) {
+      if (x > 0 && y > 0) {
+        if (can_move_to(x, y)) {
+          theInf[x][y] = "2";
+        }
+      }
+    }
+  }
 }
 
 function can_move_from(x, y) {
   return get_color(x, y) == move_color;
+}
+
+function can_move_to(x, y) {
+  if(theMap[x][y] == ' '){
+    return true
+  }
+  return get_color(x, y) != move_color;
 }
 
 function get_color(x, y) {
@@ -99,6 +121,7 @@ function figure_to_html(figure) {
 
 function show_map(board) {
   let table = document.createElement("table");
+  board.innerHTML = ''
 
   for (let y = 8; y >= 0; y--) {
     let tr = document.createElement("tr");
@@ -125,9 +148,7 @@ function show_map(board) {
         }
         td.innerHTML = figure_to_html(theMap[x][y]);
 
-        td.addEventListener('click', function(){
-            console.log(x + ' ' + y)
-          })
+        click_box(td, x, y)
       }
 
       tr.appendChild(td);
@@ -135,6 +156,43 @@ function show_map(board) {
     table.appendChild(tr);
   }
   board.appendChild(table);
+}
+
+function click_box(td, x, y){
+  td.addEventListener("click", function () {
+
+    if(theInf[x][y] == '1'){
+      click_box_from(x, y)
+    }
+    if(theInf[x][y] == '2'){
+      click_box_to(x, y)
+    }
+  });
+}
+
+function click_box_from(x, y){
+  move_from_x = x;
+  move_from_y = y;
+
+  mark_moves_to()
+  show_map(board)
+}
+
+function click_box_to(x, y){
+  theMap[x][y] = theMap[move_from_x][move_from_y]
+  theMap[move_from_x][move_from_y] = ' '
+  
+  turn_move()
+  mark_moves_from()
+  show_map(board)
+}
+
+function turn_move(){
+  if(move_color == 'white'){
+    move_color = 'black'
+  } else {
+    move_color = 'white'
+  }
 }
 
 init_map();
